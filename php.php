f090cc18ee11d25074ef29be35e721469b4b14fb
Chorshanbe <102850672+Chorshanbe@users.noreply.github.com>
2022-04-02 13:41:09 +0600

    Add files via upload

---
+++ php.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+		$x=70;
+		$y=35;
+		$z=$x/$y;
+		$q=10;
+		$num=$z*$q;
+		echo "Z= ",$z;
+		echo "NUM= ",$num;
+    ?>
+</body>
+</html>
